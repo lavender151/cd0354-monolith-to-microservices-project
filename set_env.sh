4375669e9ba474a37ad101d2f7faf2bfd13dc76f
lavender151 <trinhtan2000@gmail.com>
2022-07-15 14:22:08 +0700

    update again

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=Lavender_151
-export POSTGRES_HOST=cdr.c7ekexx8hk2f.us-east-2.rds.amazonaws.com
+export POSTGRES_HOST=tantt5-db.ca2p4d4rwxyg.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=tantt5-dev
+export AWS_BUCKET=tantt5-dev-p3
 export AWS_REGION=us-east-2
 export AWS_PROFILE=default
 export JWT_SECRET=hello
